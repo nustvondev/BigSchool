41c978ec93e61da2145d9d601e25d947d214075a
nustvondev <hoaboy89@gmail.com>
2023-03-09 15:57:39 +0700

    Add project files.

---
+++ BigSchool/Migrations/202303090814559_Add_Data_Course_Category.cs
@@ -0,0 +1,19 @@
+﻿namespace BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_Data_Course_Category : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("Insert into categories(Id,Name) values (1,'Development')");
+            Sql("Insert into categories(Id,Name) values (2,'Business')");
+            Sql("Insert into categories(Id,Name) values (3,'Marketing')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ BigSchool/ViewModels/CourseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using BigSchool.Models;
+using System.Collections.Generic;
+using System;
+
+namespace BigSchool.ViewModels
+{
+    public class CourseViewModel
+    {
+        public string Place { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
